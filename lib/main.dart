f019397f32366d8f558bfade007a51ad0027bee3
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2025-06-16 18:13:30 +0300

    small updates in layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,116 +7,278 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [
+            Row(
+              children: [
+
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                    child: GestureDetector(
+                      onTap: () => (),
+                      child: const Icon(
+                        Icons.arrow_back,
+                        size: 25.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: const Text(
+                    "Новый документ",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+
+              ],
             ),
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15),
+              child: const Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 1,
+                      child: ColoredBox(color: Color(0xFFCED6E3)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  // Основная часть экрана
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: SizedBox(
+                            width: 300,
+                            child: Image.asset('assets/img/photo.png'),
+                          ),
+                        ),
+                      ),
+
+                      // Часть с информацией о поступившем документе
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            DocumentInfoWidget(
+                              icon: Icons.person_outline,
+                              textUp: "Ирина",
+                            ),
+
+                            DocumentInfoWidget(
+                              icon: Icons.event_note,
+                              textUp: "22 мая (вт), 16:00",
+                              textDown: "Дата получения документа",
+                            ),
+
+                            DocumentInfoWidget(
+                              icon: Icons.medical_services,
+                              textUp: "Клиника «Фомина»",
+                              textDown: "Бульвар Перервинский, д. 4",
+                            ),
+
+                            DocumentInfoWidget(
+                              icon: Icons.monitor_heart,
+                              textUp: "Стоматолог",
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // Часть с описанием и кнопками
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 50),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10.0),
+                                child: SizedBox(
+                                  width: 300,
+                                  height: 115,
+                                  child: ColoredBox(
+                                    color: Color(0xfffff2d9),
+                                    child: Center(
+                                      child: Text(
+                                        "Клиника прислала документ после приёма.\nЧтобы добавить его в медкарту и\nпосмотреть содержание, нужно будет\nуказать дату рождения пациента.\nЭто проверка для безопасности данных.",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: PushButtonWidget(
+                                width: 300,
+                                height: 40,
+                                buttonColor: Color(0xff1a5dd0),
+                                textColor: Colors.white,
+                                onTapFunction: () => (),
+                                text: "Добавить в медкарту",
+                              ),
+                            ),
+
+                            PushButtonWidget(
+                              width: 300,
+                              height: 40,
+                              buttonColor: Color(0xffecf1fb),
+                              textColor: Color(0xff3470d6),
+                              fontWeight: FontWeight.bold,
+                              onTapFunction: () => (),
+                              text: "Удалить",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class DocumentInfoWidget extends StatelessWidget {
+  final IconData icon;
+  final String textUp;
+  final String? textDown;
+
+  const DocumentInfoWidget({
+    super.key,
+    required this.icon,
+    required this.textUp,
+    this.textDown,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 20,
+          height: 50,
+          child: Center(
+            child: Icon(icon, size: 25.0, color: Color(0xFF9BABBB)),
+          ),
+        ),
+
+        SizedBox(width: 32),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(textUp),
+            if (textDown != null)
+              Text(textDown!, style: TextStyle(color: Color(0xFF9BABBB))),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
+class PushButtonWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color buttonColor;
+  final Color textColor;
+  final FontWeight? fontWeight;
+  final double? fontSize;
+  final String text;
+  final void Function() onTapFunction;
+
+  const PushButtonWidget({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.buttonColor,
+    required this.textColor,
+    this.fontWeight,
+    this.fontSize,
+    required this.text,
+    required this.onTapFunction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapFunction,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: ColoredBox(
+            color: buttonColor,
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  color: textColor,
+                  fontWeight: fontWeight,
+                  fontSize: fontSize,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
